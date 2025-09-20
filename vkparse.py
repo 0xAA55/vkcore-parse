9b06a900e56b26cc4e4c90b171bb2a9a68615b06
0xAA55 <weixiaobao213@qq.com>
2025-09-20 21:47:59 +0800

    使所有的枚举类型支持 `Eq`，这样就可以作为 `HashMap` 的 `key` 了。

--- vkparse.py
+++ vkparse.py
@@ -761,7 +761,7 @@ def to_rust(outfile, parsed):
 					f.write(f'/// - Reference: <https://registry.khronos.org/vulkan/specs/latest/man/html/{enum}.html>\n')
 				f.write(feature)
 				f.write('#[repr(C)]\n')
-				f.write('#[derive(Debug, Clone, Copy, PartialEq)]\n')
+				f.write('#[derive(Debug, Clone, Copy, PartialEq, Eq)]\n')
 				f.write('#[allow(non_camel_case_types)]\n')
 				f.write(f'pub enum {enum} {{\n')
 				for enumname, enumval in enumpair.items():
